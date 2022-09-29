8a8e6550798f7ae023a2a6496da718e35360fd1a
DEV <evgenii.ganichev@gmail.com>
2022-09-29 14:31:53 +0300

    initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import Home from 'pages/home/container';
+import React from 'react';
+
+import './styles/index.scss';
+
+const App = () => {
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/pages/home/container/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const Home = () => {
+	return <div className={styles.root}>Home</div>;
+};
+
+export default Home;
